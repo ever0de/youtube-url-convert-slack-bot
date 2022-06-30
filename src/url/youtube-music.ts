6f2e0e33928966cb401c12465c74d6457b0755d3
Jiseok CHOI <jiseok.dev@gmail.com>
2022-06-30 20:22:59 +0900

    Revert youtube music url

--- src/url/youtube-music.ts
+++ src/url/youtube-music.ts
@@ -1,9 +1,8 @@
 export const getYoutubeURLFromYoutubeMusic = async (url: string) => {
   const youtubeURL = url.replace("music.", "");
-  const [splitURL, content] = youtubeURL.split(/\n| /);
 
   if (youtubeURL.includes("&list")) {
-    return [splitURL.slice(0, youtubeURL.indexOf("&list")), content].join("\n");
+    return youtubeURL.slice(0, youtubeURL.indexOf("&list"));
   }
 
   return youtubeURL;
